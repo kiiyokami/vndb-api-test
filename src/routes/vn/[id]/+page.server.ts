d71ce5cd5199b84f289e9b83999b4e052da01d2d
Inovvia <70137651+Inovvia@users.noreply.github.com>
2023-06-19 22:07:09 +0800

    fix carousel

--- src/routes/vn/[id]/+page.server.ts
+++ src/routes/vn/[id]/+page.server.ts
@@ -1,28 +1,27 @@
 export async function load({ params }) {
-	console.log(params);
-	const requestHeaders = new Headers();
-	requestHeaders.append("Content-Type", "application/json");
-	
-	const raw = JSON.stringify({
-	  "filters": ["id","=",params.id],
-	  "fields": "title, image.url, description, rating, screenshots.url"
-	});
-	
-	let jsonResponse : any;
+  console.log(params);
+  const requestHeaders = new Headers();
+  requestHeaders.append("Content-Type", "application/json");
 
-	try{
-		const response = await fetch("https://api.vndb.org/kana/vn",{
-			method: 'POST',
-			headers: requestHeaders,
-			body: raw,
-			redirect: 'follow' as RequestRedirect
-		  })
-		  jsonResponse = await response.json();
-    	const results = jsonResponse.results[0];
-		return { vnInfo: results }
-		}catch (error) {
-		console.log(error);
-		return { vnInfo: null };
-	  }
+  const raw = JSON.stringify({
+    filters: ["id", "=", params.id],
+    fields: "title, image.url, description, rating, screenshots.url",
+  });
+
+  let jsonResponse: any;
+
+  try {
+    const response = await fetch("https://api.vndb.org/kana/vn", {
+      method: "POST",
+      headers: requestHeaders,
+      body: raw,
+      redirect: "follow" as RequestRedirect,
+    });
+    jsonResponse = await response.json();
+    const results = jsonResponse.results[0];
+    return { vnInfo: results };
+  } catch (error) {
+    console.log(error);
+    return { vnInfo: null };
   }
-  
+}
--- src/routes/vn/[id]/+page.svelte
+++ src/routes/vn/[id]/+page.svelte
@@ -1,47 +1,46 @@
 <script>
-// @ts-nocheck
-	import Carousel from "svelte-carousel";
-	export let data;
-	let carousel; // for calling methods of the carousel instance
-	const handleNextClick = () => {
-		carousel.goToNext();
-	};
+  // @ts-nocheck
+  import Carousel from "svelte-carousel";
+  import { browser } from "$app/environment";
+  export let data;
+  let carousel; // for calling methods of the carousel instance
+  const handleNextClick = () => {
+    carousel.goToNext();
+  };
 </script>
-  
-  <main>
-	{#if data }
-	  <h1 class="text-center font-semibold text-2xl">{data.vnInfo.title}</h1>
-	  <img src={data.vnInfo.image.url} alt="">
-	  <div>{@html data.vnInfo.description.replace(/\[[^\]]*\]/g, '')}</div>
-	{:else}
-	  <p>Loading...</p>
-	{/if}
 
-	
-	<div class="carousel">
-	<Carousel
-    bind:this={carousel}
-    let:loaded
-    autoplay
-    autoplayDuration={3000}
-    autoplayProgressVisible
-	
->	
-    {#each data.vnInfo.screenshots as src, imageIndex (src)}
-      <div class="image-container ">
-        {#if loaded.includes(imageIndex)}
-			<img class="image" src={src.url} alt='content' width={600} />
-        {/if}
-      </div>
-    {/each}
-</Carousel>
-</div>
-  </main>
+<main>
+  {#if data.vnInfo !== null}
+    <h1 class="text-center font-semibold text-2xl">{data.vnInfo.title}</h1>
+    <img src={data.vnInfo.image.url} alt="" />
+    <div>{@html data.vnInfo.description.replace(/\[[^\]]*\]/g, "")}</div>
 
-  <style>
-	.carousel{
-		width: 600px;
-	}
-  </style>
-  
-  
+    <div class="carousel">
+      {#if browser}
+        <Carousel
+          bind:this={carousel}
+          let:loaded
+          autoplay
+          autoplayDuration={3000}
+          autoplayProgressVisible
+        >
+          {#each data.vnInfo.screenshots as src, imageIndex (src)}
+            <div class="image-container">
+              {#if loaded.includes(imageIndex)}
+                <img class="image" src={src.url} alt="content" width={600} />
+              {/if}
+            </div>
+          {/each}
+        </Carousel>
+      {/if}
+    </div>
+  {:else}
+    <p>Loading...</p>
+  {/if}
+</main>
+
+<style>
+  .carousel {
+    width: 600px;
+  }
+</style>
