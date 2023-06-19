4d2aa70ba88f21e4584e9bebdfb4ab266c5a9eb0
Inovvia <70137651+Inovvia@users.noreply.github.com>
2023-06-19 20:38:48 +0800

    Fix 500 error for list page and refactor list func

--- src/modules/datalist.ts
+++ src/modules/datalist.ts
@@ -1,32 +1,33 @@
-var myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
+export async function getListofVNs() {
+  const requestHeaders = new Headers();
+  requestHeaders.append("Content-Type", "application/json");
+  const raw = JSON.stringify({
+    user: "u225866",
+    fields: "id, vote,vn.id, vn.title",
+    filters: ["label", "=", 7],
+    sort: "vote",
+    reverse: true,
+    results: 30,
+  });
+  let vnArray: any = [];
+  let jsonResponse: any;
+  try {
+    const response = await fetch("https://api.vndb.org/kana/ulist", {
+      headers: requestHeaders,
+      body: raw,
+      redirect: "follow" as RequestRedirect,
+      method: "POST",
+    });
+    jsonResponse = await response.json();
+    const result = jsonResponse.results;
 
-var raw = JSON.stringify({
-  "user": "u225866",
-  "fields": "id, vote,vn.id, vn.title",
-  "filters": [
-    "label",
-    "=",
-    7
-  ],
-  "sort": "vote",
-  "reverse": true,
-  "results": 30
-});
-
-var requestOptions: RequestInit = {
-    method: 'POST',
-    headers: myHeaders,
-    body: raw,
-    redirect: 'follow' as RequestRedirect,
-  };
-  
-  fetch("https://api.vndb.org/kana/ulist", requestOptions)
-    .then(response => response.json())
-    .then(data => {
-      vnList = data;
-
-    })
-    .catch(error => console.log('error', error));
-
-    export let vnList: any;
+    for (let i = 0; i < result.length; i++) {
+      const element = result[i];
+      vnArray.push({ id: element.id, title: element.vn.title });
+    }
+    return vnArray;
+  } catch (error) {
+    console.log(error);
+    return vnArray;
+  }
+}
